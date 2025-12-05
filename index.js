714936f31d88dd930283051a5a4325d3d778e01a
sh1ckdev <makerofthefuture@gmail.com>
2025-12-05 20:03:20 +0300

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require("express");
+const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// раздача статических файлов
 app.use(express.static(__dirname));
 
-// если будешь добавлять css/js — сервер отдает их тоже
-// app.use(express.static(path.join(__dirname, "public")));
-
-app.get("*", (req, res) => {
+app.get("*", (req,res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`🚀 Emulator running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`🚀 Running on http://localhost:${PORT}`));
